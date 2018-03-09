4ce9764eca743fd3a623cbe06e070f985049bbb5
volummidia <32236323+volummidia@users.noreply.github.com>
2018-03-09 13:29:53 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,30 +1,56 @@
 
 			function seg() {
 				var dia= document.getElementById("dia");
-				dia.innerHTML="<h4>Segunda-Feira</h4><p>Das 6H às 8H - Abrindo as porteiras com música gaúcha - Apresentação de <a href=''>carlinhos</a></p><p>Das 8H às 11H - Show da manhâ com mix de musicas - Apresentação de <a href=''>Cristianu</a></p><p>Das 11H às 11:30H - Especial do artista - Apresentação de <a href=''>gilberto</a></p><p>Das 12H às 13H - A hora do Brick - Apresentação de <a href=''>cristiano</a></p><p>Das 13H às 14H - Show de bandas - Apresentação de <a href=''>jusefo</a></p><p>Das 14H às 15:30H - Pop Rock nacional e internacional - Apresentação de <a href=''>diego</a></p><p>Das 17H às 18H - Coração Sertanejo - Apresentação de <a href=''>andrei</a></p><p>Das 6H às 8H - Abrindo as porteiras com música gaúcha - Apresentação de <a href=''>jusefo gay</a></p>";
+				dia.innerHTML="<h4>Segunda-Feira</h4><p>Das 6H Ã s 8H - Abrindo as porteiras com mÃºsica gaÃºcha - ApresentaÃ§Ã£o de <a href=''>carlinhos</a></p><p>Das 8H Ã s 11H - Show da manhÃ¢ com mix de musicas - ApresentaÃ§Ã£o de <a href=''>Cristianu</a></p><p>Das 11H Ã s 11:30H - Especial do artista - ApresentaÃ§Ã£o de <a href=''>gilberto</a></p><p>Das 12H Ã s 13H - A hora do Brick - ApresentaÃ§Ã£o de <a href=''>cristiano</a></p><p>Das 13H Ã s 14H - Show de bandas - ApresentaÃ§Ã£o de <a href=''>jusefo</a></p><p>Das 14H Ã s 15:30H - Pop Rock nacional e internacional - ApresentaÃ§Ã£o de <a href=''>diego</a></p><p>Das 17H Ã s 18H - CoraÃ§Ã£o Sertanejo - ApresentaÃ§Ã£o de <a href=''>andrei</a></p><p>Das 6H Ã s 8H - Abrindo as porteiras com mÃºsica gaÃºcha - ApresentaÃ§Ã£o de <a href=''>jusefo gay</a></p>";
 			}			
 			function ter() {
 				var dia= document.getElementById("dia");
-				dia.innerHTML="<h4>Terça-Feira</h4><p>Das 6H às 8H - Abrindo as porteiras com música gaúcha - Apresentação de <a href=''>carlinhos</a></p><p>Das 8H às 11H - Show da manhâ com mix de musicas - Apresentação de <a href=''>Cristianu</a></p><p>Das 11H às 11:30H - Especial do artista - Apresentação de <a href=''>gilberto</a></p><p>Das 12H às 13H - Jornal - Apresentação de <a href=''>cristiano</a></p><p>Das 13H às 14H - Show de bandas - Apresentação de <a href=''>jusefo</a></p><p>Das 14H às 15:30H - Pop Rock nacional e internacional - Apresentação de <a href=''>diego</a></p><p>Das 17H às 18H - Coração Sertanejo - Apresentação de <a href=''>andrei</a></p><p>Das 6H às 8H - Abrindo as porteiras com música gaúcha - Apresentação de <a href=''>jusefo gay</a></p>";
+				dia.innerHTML="<h4>TerÃ§a-Feira</h4><p>Das 6H Ã s 8H - Abrindo as porteiras com mÃºsica gaÃºcha - ApresentaÃ§Ã£o de <a href=''>carlinhos</a></p><p>Das 8H Ã s 11H - Show da manhÃ¢ com mix de musicas - ApresentaÃ§Ã£o de <a href=''>Cristianu</a></p><p>Das 11H Ã s 11:30H - Especial do artista - ApresentaÃ§Ã£o de <a href=''>gilberto</a></p><p>Das 12H Ã s 13H - Jornal - ApresentaÃ§Ã£o de <a href=''>cristiano</a></p><p>Das 13H Ã s 14H - Show de bandas - ApresentaÃ§Ã£o de <a href=''>jusefo</a></p><p>Das 14H Ã s 15:30H - Pop Rock nacional e internacional - ApresentaÃ§Ã£o de <a href=''>diego</a></p><p>Das 17H Ã s 18H - CoraÃ§Ã£o Sertanejo - ApresentaÃ§Ã£o de <a href=''>andrei</a></p><p>Das 6H Ã s 8H - Abrindo as porteiras com mÃºsica gaÃºcha - ApresentaÃ§Ã£o de <a href=''>jusefo gay</a></p>";
 			}			
 			function qua() {
 				var dia= document.getElementById("dia");
-				dia.innerHTML="<h4>Quarta-Feira</h4><p>Das 6H às 8H - Abrindo as porteiras com música gaúcha - Apresentação de <a href=''>carlinhos</a></p><p>Das 8H às 11H - Show da manhâ com mix de musicas - Apresentação de <a href=''>Cristianu</a></p><p>Das 11H às 11:30H - Especial do artista - Apresentação de <a href=''>gilberto</a></p><p>Das 12H às 13H - A hora do Brick - Apresentação de <a href=''>cristiano</a></p><p>Das 13H às 14H - Show de bandas - Apresentação de <a href=''>jusefo</a></p><p>Das 14H às 15:30H - Pop Rock nacional e internacional - Apresentação de <a href=''>diego</a></p><p>Das 17H às 18H - Coração Sertanejo - Apresentação de <a href=''>andrei</a></p><p>Das 6H às 8H - Abrindo as porteiras com música gaúcha - Apresentação de <a href=''>jusefo gay</a></p>";
+				dia.innerHTML="<h4>Quarta-Feira</h4><p>Das 6H Ã s 8H - Abrindo as porteiras com mÃºsica gaÃºcha - ApresentaÃ§Ã£o de <a href=''>carlinhos</a></p><p>Das 8H Ã s 11H - Show da manhÃ¢ com mix de musicas - ApresentaÃ§Ã£o de <a href=''>Cristianu</a></p><p>Das 11H Ã s 11:30H - Especial do artista - ApresentaÃ§Ã£o de <a href=''>gilberto</a></p><p>Das 12H Ã s 13H - A hora do Brick - ApresentaÃ§Ã£o de <a href=''>cristiano</a></p><p>Das 13H Ã s 14H - Show de bandas - ApresentaÃ§Ã£o de <a href=''>jusefo</a></p><p>Das 14H Ã s 15:30H - Pop Rock nacional e internacional - ApresentaÃ§Ã£o de <a href=''>diego</a></p><p>Das 17H Ã s 18H - CoraÃ§Ã£o Sertanejo - ApresentaÃ§Ã£o de <a href=''>andrei</a></p><p>Das 6H Ã s 8H - Abrindo as porteiras com mÃºsica gaÃºcha - ApresentaÃ§Ã£o de <a href=''>jusefo gay</a></p>";
 			}			
 			function qui() {
 				var dia= document.getElementById("dia");
-				dia.innerHTML="<h4>Quinta-Feira</h4><p>Das 6H às 8H - Abrindo as porteiras com música gaúcha - Apresentação de <a href=''>carlinhos</a></p><p>Das 8H às 11H - Show da manhâ com mix de musicas - Apresentação de <a href=''>Cristianu</a></p><p>Das 11H às 11:30H - Especial do artista - Apresentação de <a href=''>gilberto</a></p><p>Das 12H às 13H - Jornal - Apresentação de <a href=''>cristiano</a></p><p>Das 13H às 14H - Show de bandas - Apresentação de <a href=''>jusefo</a></p><p>Das 14H às 15:30H - Pop Rock nacional e internacional - Apresentação de <a href=''>diego</a></p><p>Das 17H às 18H - Coração Sertanejo - Apresentação de <a href=''>andrei</a></p><p>Das 6H às 8H - Abrindo as porteiras com música gaúcha - Apresentação de <a href=''>jusefo gay</a></p>";
+				dia.innerHTML="<h4>Quinta-Feira</h4><p>Das 6H Ã s 8H - Abrindo as porteiras com mÃºsica gaÃºcha - ApresentaÃ§Ã£o de <a href=''>carlinhos</a></p><p>Das 8H Ã s 11H - Show da manhÃ¢ com mix de musicas - ApresentaÃ§Ã£o de <a href=''>Cristianu</a></p><p>Das 11H Ã s 11:30H - Especial do artista - ApresentaÃ§Ã£o de <a href=''>gilberto</a></p><p>Das 12H Ã s 13H - Jornal - ApresentaÃ§Ã£o de <a href=''>cristiano</a></p><p>Das 13H Ã s 14H - Show de bandas - ApresentaÃ§Ã£o de <a href=''>jusefo</a></p><p>Das 14H Ã s 15:30H - Pop Rock nacional e internacional - ApresentaÃ§Ã£o de <a href=''>diego</a></p><p>Das 17H Ã s 18H - CoraÃ§Ã£o Sertanejo - ApresentaÃ§Ã£o de <a href=''>andrei</a></p><p>Das 6H Ã s 8H - Abrindo as porteiras com mÃºsica gaÃºcha - ApresentaÃ§Ã£o de <a href=''>jusefo gay</a></p>";
 			}			
 			function sex() {
 				var dia= document.getElementById("dia");
-				dia.innerHTML="<h4>Sexta-Feira</h4><p>Das 6H às 8H - Abrindo as porteiras com música gaúcha - Apresentação de <a href=''>carlinhos</a></p><p>Das 8H às 11H - Show da manhâ com mix de musicas - Apresentação de <a href=''>Cristianu</a></p><p>Das 11H às 11:30H - Especial do artista - Apresentação de <a href=''>gilberto</a></p><p>Das 12H às 13H - Jornal - Apresentação de <a href=''>cristiano</a></p><p>Das 13H às 14H - Show de bandas - Apresentação de <a href=''>jusefo</a></p><p>Das 14H às 15:30H - Pop Rock nacional e internacional - Apresentação de <a href=''>diego</a></p><p>Das 17H às 18H - Coração Sertanejo - Apresentação de <a href=''>andrei</a></p><p>Das 6H às 8H - Abrindo as porteiras com música gaúcha - Apresentação de <a href=''>jusefo gay</a></p>";
+				dia.innerHTML="<h4>Sexta-Feira</h4><p>Das 6H Ã s 8H - Abrindo as porteiras com mÃºsica gaÃºcha - ApresentaÃ§Ã£o de <a href=''>carlinhos</a></p><p>Das 8H Ã s 11H - Show da manhÃ¢ com mix de musicas - ApresentaÃ§Ã£o de <a href=''>Cristianu</a></p><p>Das 11H Ã s 11:30H - Especial do artista - ApresentaÃ§Ã£o de <a href=''>gilberto</a></p><p>Das 12H Ã s 13H - Jornal - ApresentaÃ§Ã£o de <a href=''>cristiano</a></p><p>Das 13H Ã s 14H - Show de bandas - ApresentaÃ§Ã£o de <a href=''>jusefo</a></p><p>Das 14H Ã s 15:30H - Pop Rock nacional e internacional - ApresentaÃ§Ã£o de <a href=''>diego</a></p><p>Das 17H Ã s 18H - CoraÃ§Ã£o Sertanejo - ApresentaÃ§Ã£o de <a href=''>andrei</a></p><p>Das 6H Ã s 8H - Abrindo as porteiras com mÃºsica gaÃºcha - ApresentaÃ§Ã£o de <a href=''>jusefo gay</a></p>";
 			}			
 			function sab() {
 				var dia= document.getElementById("dia");
-				dia.innerHTML="<h4>Sábado</h4><p>Das 6H às 8H - Abrindo as porteiras com música gaúcha - Apresentação de <a href=''>carlinhos</a></p><p>Das 8H às 11H - Show da manhâ com mix de musicas - Apresentação de <a href=''>Cristianu</a></p><p>Das 11H às 11:30H - Especial do artista - Apresentação de <a href=''>gilberto</a></p><p>Das 12H às 13H - Outra coisa - Apresentação de <a href=''>cristiano</a></p><p>Das 13H às 14H - Show de bandas - Apresentação de <a href=''>jusefo</a></p><p>Das 14H às 15:30H - Pop Rock nacional e internacional - Apresentação de <a href=''>diego</a></p><p>Das 17H às 18H - Coração Sertanejo - Apresentação de <a href=''>andrei</a></p><p>Das 6H às 8H - Abrindo as porteiras com música gaúcha - Apresentação de <a href=''>jusefo gay</a></p>";
+				dia.innerHTML="<h4>SÃ¡bado</h4><p>Das 6H Ã s 8H - Abrindo as porteiras com mÃºsica gaÃºcha - ApresentaÃ§Ã£o de <a href=''>carlinhos</a></p><p>Das 8H Ã s 11H - Show da manhÃ¢ com mix de musicas - ApresentaÃ§Ã£o de <a href=''>Cristianu</a></p><p>Das 11H Ã s 11:30H - Especial do artista - ApresentaÃ§Ã£o de <a href=''>gilberto</a></p><p>Das 12H Ã s 13H - Outra coisa - ApresentaÃ§Ã£o de <a href=''>cristiano</a></p><p>Das 13H Ã s 14H - Show de bandas - ApresentaÃ§Ã£o de <a href=''>jusefo</a></p><p>Das 14H Ã s 15:30H - Pop Rock nacional e internacional - ApresentaÃ§Ã£o de <a href=''>diego</a></p><p>Das 17H Ã s 18H - CoraÃ§Ã£o Sertanejo - ApresentaÃ§Ã£o de <a href=''>andrei</a></p><p>Das 6H Ã s 8H - Abrindo as porteiras com mÃºsica gaÃºcha - ApresentaÃ§Ã£o de <a href=''>jusefo gay</a></p>";
 			}			
 			function dom() {
 				var dia= document.getElementById("dia");
-				dia.innerHTML="<h4>Domingo</h4><p>Das 6H às 8H - Abrindo as porteiras com música gaúcha - Apresentação de <a href=''>carlinhos</a></p><p>Das 8H às 11H - Show da manhâ com mix de musicas - Apresentação de <a href=''>Cristianu</a></p><p>Das 11H às 11:30H - Especial do artista - Apresentação de <a href=''>gilberto</a></p><p>Das 12H às 13H - Outra coisa - Apresentação de <a href=''>cristiano</a></p><p>Das 13H às 14H - Show de bandas - Apresentação de <a href=''>jusefo</a></p><p>Das 14H às 15:30H - Pop Rock nacional e internacional - Apresentação de <a href=''>diego</a></p><p>Das 17H às 18H - Coração Sertanejo - Apresentação de <a href=''>andrei</a></p><p>Das 6H às 8H - Abrindo as porteiras com música gaúcha - Apresentação de <a href=''>jusefo gay</a></p>";
+				dia.innerHTML="<h4>Domingo</h4><p>Das 6H Ã s 8H - Abrindo as porteiras com mÃºsica gaÃºcha - ApresentaÃ§Ã£o de <a href=''>carlinhos</a></p><p>Das 8H Ã s 11H - Show da manhÃ¢ com mix de musicas - ApresentaÃ§Ã£o de <a href=''>Cristianu</a></p><p>Das 11H Ã s 11:30H - Especial do artista - ApresentaÃ§Ã£o de <a href=''>gilberto</a></p><p>Das 12H Ã s 13H - Outra coisa - ApresentaÃ§Ã£o de <a href=''>cristiano</a></p><p>Das 13H Ã s 14H - Show de bandas - ApresentaÃ§Ã£o de <a href=''>jusefo</a></p><p>Das 14H Ã s 15:30H - Pop Rock nacional e internacional - ApresentaÃ§Ã£o de <a href=''>diego</a></p><p>Das 17H Ã s 18H - CoraÃ§Ã£o Sertanejo - ApresentaÃ§Ã£o de <a href=''>andrei</a></p><p>Das 6H Ã s 8H - Abrindo as porteiras com mÃºsica gaÃºcha - ApresentaÃ§Ã£o de <a href=''>jusefo gay</a></p>";
 			}
-		
+		  function diasemana(){
+        var hoje=new Date();
+        var dia= hoje.getDay();
+        var semana=new Array(6);
+          semana[0]=0;
+          semana[1]=1;
+          semana[2]=2;
+          semana[3]=3;
+          semana[4]=4;
+          semana[5]=5;
+          semana[6]=6;
+        switch (semana[dia]) {
+          case 1 : seg();
+            break;
+          case 2 : ter();
+            break;
+          case 3 : qua();
+            break;
+          case 4 : qui();
+            break;
+          case 5 : sex();
+            break;
+          case 6 : sab();
+            break;
+          case 0 : dom();
+        }
+      }
